feda3474483dcfb5e01da1257d804c1e00296f17
Student <example@example.com>
2019-07-19 13:43:53 -0700

    checks that noth words are the same length

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,16 +9,24 @@ class Anagram
     @array2 = word2.downcase.split('')
     @vowels = ['a', 'e', 'i', 'o', 'u', 'y']
   end
-  def is_word?
+  def is_word
     @array1.each do |letter|
       if @vowels.include?(letter)
         return "Word one has at least one vowel"
       end
     end
   end
-  def word2_is_anagram?
+  def word2_is_anagram
     if @word1 == @word2
       return "that's the same word!"
+    elsif @array1.length != @array2.length
+      return "That's not an anagram"
+    else
+      @array2.all? do |letter|
+        if @array1.include?(letter)
+          return "You have an anagram!"
+        end
+      end
     end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,10 +4,14 @@ require ('Anagram')
 describe('#Anagram') do
   it('checks that word1 contains at least one vowel') do
     anagram = Anagram.new("Dog", "God")
-  expect(anagram.is_word?).to(eq("Word one has at least one vowel"))
+  expect(anagram.is_word).to(eq("Word one has at least one vowel"))
   end
   it("checks that word one and word two aren't the same word") do
     anagram = Anagram.new("God", "God")
-  expect(anagram.word2_is_anagram?).to(eq("that's the same word!"))
+  expect(anagram.word2_is_anagram).to(eq("that's the same word!"))
+  end
+  it("should tell if word two is not an anagram of word one") do
+    anagram = Anagram.new("Gods", "Dog")
+  expect(anagram.word2_is_anagram).to(eq("That's not an anagram"))
   end
 end
