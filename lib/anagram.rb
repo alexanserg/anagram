a2cfb2f238ab788be5ed2cdb2a1459335ba3a211
Student <example@example.com>
2019-07-19 13:17:10 -0700

    check that the two words aren't the same word

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,22 +3,22 @@ require 'pry'
 class Anagram
   attr_accessor(:word1, :word2)
   def initialize(word1, word2)
-    @word1 = word1.downcase.split('')
-    @word2 = word2.downcase.split('')
+    @word1 = word1
+    @array1 = word1.downcase.split('')
+    @word2 = word2
+    @array2 = word2.downcase.split('')
     @vowels = ['a', 'e', 'i', 'o', 'u', 'y']
   end
-  def is_word1?
-    @word1.each do |letter|
+  def is_word?
+    @array1.each do |letter|
       if @vowels.include?(letter)
         return "Word one has at least one vowel"
       end
     end
   end
-  def is_word2?
-    @word2.each do |letter|
-      if @vowels.include?(letter)
-        return "Word two has at least one vowel"
-      end
+  def word2_is_anagram?
+    if @word1 == @word2
+      return "that's the same word!"
     end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,10 +2,12 @@ require ('rspec')
 require ('Anagram')
 
 describe('#Anagram') do
-anagram = Anagram.new("Dog", "God")
   it('checks that word1 contains at least one vowel') do
-     anagram.is_word1?
-  expect(anagram.is_word1?).to(eq("Word one has at least one vowel"))
- end
-
+    anagram = Anagram.new("Dog", "God")
+  expect(anagram.is_word?).to(eq("Word one has at least one vowel"))
+  end
+  it("checks that word one and word two aren't the same word") do
+    anagram = Anagram.new("God", "God")
+  expect(anagram.word2_is_anagram?).to(eq("that's the same word!"))
+  end
 end
